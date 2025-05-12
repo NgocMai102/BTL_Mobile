bc48c5e5e83cd538165fb56fc3aee75ab944e0fa
happyqq833 <happyqq2003@gmail.com>
2025-05-13 01:11:57 +0700

    finish create menu

--- app/src/main/java/com/example/btl_android/View/menu/MenuFragment.java
+++ app/src/main/java/com/example/btl_android/View/menu/MenuFragment.java
@@ -1,17 +1,95 @@
 package com.example.btl_android.View.menu;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MenuFragment extends Fragment {
-    public MenuFragment() { }
+import com.example.btl_android.R;
+import com.example.btl_android.View.history_sharing.HistoryShareMoneyActivity;
+import com.example.btl_android.View.searchmoney.ShareMoneyActivity;
+import com.example.btl_android.Components.DataBaseManager;
+import com.example.btl_android.databinding.FragmentMenuBinding;
 
+import java.util.Calendar;
+
+public class MenuFragment extends Fragment {
+    private FragmentMenuBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = FragmentMenuBinding.inflate(getLayoutInflater());
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return new View(getContext()); // return view trống
+        onClick();
+        return binding.getRoot();
     }
+
+    private void onClick() {
+        binding.lnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                delete();
+            }
+
+        });
+        binding.lnShareMoney.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareMoney();
+            }
+        });
+        binding.lnHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                history();
+            }
+        });
+    }
+
+    private void history() {
+        Log.d("MenuFragment", "Opening HistoryShareMoneyActivity...");
+        startActivity(new Intent(requireContext(), HistoryShareMoneyActivity.class));
+    }
+
+    private void shareMoney() {
+        startActivity(new Intent(requireContext(), ShareMoneyActivity.class));
+    }
+
+    private void delete() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Câu hỏi")
+                .setMessage(
+                        "Bạn có chắc chắn muốn xóa tất cả dữ liệu? Thao tác này không thể\n" +
+                                "hoàn tác lại."
+                )
+                .setPositiveButton("Yes", (dialog, which) -> {
+                    try {
+                        DataBaseManager.deleteAllTable();
+
+                    } catch (Exception e) {
+                        e.getMessage();
+
+                    }
+
+                    dialog.dismiss();
+                })
+                .setNegativeButton("No", (dialog, which) -> {
+                    dialog.dismiss();
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+
 }
