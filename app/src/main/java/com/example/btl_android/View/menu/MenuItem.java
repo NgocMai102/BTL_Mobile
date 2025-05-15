6571af34a70b059e15b5481dc1d93bb632f21e39
NgocMai102 <mai.pham.1022k3@gmail.com>
2025-05-16 00:18:23 +0700

    fix

---
+++ app/src/main/java/com/example/btl_android/View/menu/MenuItem.java
@@ -0,0 +1,19 @@
+package com.example.btl_android.View.menu;
+
+public class MenuItem {
+    private int iconResId;
+    private String title;
+
+    public MenuItem(int iconResId, String title) {
+        this.iconResId = iconResId;
+        this.title = title;
+    }
+
+    public int getIconResId() {
+        return iconResId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
